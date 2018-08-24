3f89cbed07580dce6b9a3bb625ea5dce0c71b8fb
rpmeer <reinanpmeer@gmail.com>
2018-08-24 20:05:58 +0800

    MainActivity updated

--- PlaySafe/app/src/main/java/apc/edu/ph/playsafe/MainActivity.java
+++ PlaySafe/app/src/main/java/apc/edu/ph/playsafe/MainActivity.java
@@ -1,60 +1,40 @@
-package apc.edu.ph.playsafe;
+MainActivity.java
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import com.bumptech.glide.Glide;
-import com.facebook.Profile;
-import com.facebook.login.LoginManager;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.widget.EditText;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+public class MainActivity extends Activity {
 
-
-public class MainActivity extends AppCompatActivity {
-
-    private TextView nameTextView;
-    private CircleImageView imageView;
+    EditText ET_NAME,ET_PASS;
+    String login_name,login_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameTextView = (TextView) findViewById(R.id.nameTextView);
-        imageView = (CircleImageView) findViewById(R.id.fbimage);
+        ET_NAME = (EditText)findViewById(R.id.user_name);
+        ET_PASS = (EditText)findViewById(R.id.user_pass);
+    }
+    public void userReg(View view)
+    {
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-        if (user != null) {
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            String image_url = "http://graph.facebook.com/" + Profile.getCurrentProfile().getId() + "/picture?type=large";
-            String uid = user.getUid();
-
-
-            nameTextView.setText(name);
-            Glide.with(this)
-                    .load(image_url)
-                    .into(imageView);
-        } else {
-            goLoginScreen();
-        }
+        startActivity(new Intent(this,Register.class));
     }
 
-    private void goLoginScreen() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
+    public void userLogin(View view)
+    {
+        login_name = ET_NAME.getText().toString();
+        login_pass = ET_PASS.getText().toString();
+        String method = "login";
+        BackgroundTask backgroundTask = new BackgroundTask(this);
+        backgroundTask.execute(method,login_name,login_pass);
 
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();
-        LoginManager.getInstance().logOut();
-        goLoginScreen();
     }
-}
